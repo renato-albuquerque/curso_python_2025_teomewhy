0efae6a0bad6c65d4dbe1a75c490ba2ea9a55946
renato_albuquerque <renatomalb@hotmail.com>
2025-04-06 22:35:11 -0300

    first commit

---
+++ 01_ola_mundo.py
@@ -0,0 +1,7 @@
+# %%
+print("Hello World !!!")
+
+# %%
+# Comentário 2
+print("Good morning")
+print("Good night")
---
+++ 03_variaveis.py
@@ -0,0 +1,10 @@
+# %%
+nome = "Renato"
+print(nome)
+
+nome = "Garrafa de água"
+print(nome)
+
+# %%
+soma = 1+1
+print("1 + 1 =", soma)
